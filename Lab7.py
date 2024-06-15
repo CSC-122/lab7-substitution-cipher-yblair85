2925771a7f22b4eb39659b07104a53f6cd34946e
Yulia Blair <yblair@umich.edu>
2024-06-14 20:31:00 -0400

    yes

--- Lab7.py
+++ Lab7.py
@@ -1,6 +1,6 @@
 # CSC 122
 # Substitution Cipher
-# By <Your Name>
+# By <Yulia Blair>
 
 """
 Inputs, Processes, Outputs (IOP)
@@ -11,7 +11,19 @@ Output(s):
 """
 
 def main():
-    pass # replace this line with all of your solution code
+    cipher = []
+    message = input("Enter the message to be encrypted: ")
+    for letter in message:
+        cipher.append(chr(ord(letter) + 3))
+    print(f"Encrypted message = {''.join(cipher)}\n")
+
+    plaintext = []
+    message = input("Enter the cipher to be decrypted: ")
+    for letter in message:
+        plaintext.append(chr(ord(letter) - 3))
+    print(f"Decrypted message = {''.join(plaintext)}\n")
+
+
 
 
 if __name__ == '__main__':
